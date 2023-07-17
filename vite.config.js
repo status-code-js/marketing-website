7828d7da804ba66bea7848680a9e75b99b454f8a
status-code-js <sonnenuntergang.email@gmail.com>
2023-07-17 14:14:48 +0200

    feat: upload project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://status-code-js.github.io/marketing-website",
   plugins: [react()],
 })

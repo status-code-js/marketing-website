fa20711ac9eb8b92c2d3e4c12e04f1116343fb72
status-code-js <sonnenuntergang.email@gmail.com>
2023-07-17 15:36:43 +0200

    feat: update project

--- src/data/index.js
+++ src/data/index.js
@@ -16,21 +16,21 @@ export const testimonials = [
         quote: "The team's professionalism, attention to detail, and commitment to our success are truly commendable.",
         author: "Mary Smith",
         position: "Designer",
-        photo: "/assets/testimonial-1.png"
+        photo: "assets/testimonial-1.png"
     },
     {
         id: 2,
         quote: "It exceeded my expectations. The marketing campaign created by the agency really met my requirements. Keep up the excellent work!",
         author: "Tom Wilson",
         position: "Developer",
-        photo: "/assets/testimonial-2.png"
+        photo: "assets/testimonial-2.png"
     },
     {
         id: 3,
         quote: "It really met my requirements. If somebody were to ask me for something similar, I would definitely recommend you guys!",
         author: "Sara Dylan",
         position: "Product Manager",
-        photo: "/assets/testimonial-3.png"
+        photo: "assets/testimonial-3.png"
     }
 ]
 
